e5c46a1a4da2d960dcd5667e85bff34a62bd09a4
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-15 16:53:29 -0400

    webpack setup completed

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,3 +1,12 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client'
 
 const test = <p>Hello World!</p>
+
+//create root element
+const rootElement = document.createElement('div')
+document.body.appendChild(rootElement)
+
+//create a root and render the component
+const root = createRoot(rootElement)
+root.render(test)
