6bf8049bddecb21cd96cdac55d737c441a93ab51
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-15 18:34:40 -0400

    background and content script added

---
+++ src/background/background.ts
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  console.log('msg :>> ', msg)
+  console.log('sender :>> ', sender)
+  sendResponse('From the background Script!')
+})
